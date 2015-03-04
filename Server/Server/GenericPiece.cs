07a9248f5b53929091fa71319fe261e03b529497
jheckard <jason.heckard@yahoo.com>
2015-03-03 18:18:44 -0800

    Began master class game logic on server
    GenericGame and GenericPiece exist on server; not fully finished yet

---
+++ Server/Server/GenericPiece.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class GenericPiece
+    {
+        private int x_pos;
+        private int y_pos;
+
+        public void setPosition(int newX, int newY)
+        {
+            setX(newX);
+            setY(newY);
+        }
+
+        public int getX()
+        {
+            return x_pos;
+        }
+
+        public void setX(int newX)
+        {
+            x_pos = newX; 
+        }
+
+        public int getY()
+        {
+            return y_pos;
+        }
+
+        public void setY(int newY)
+        {
+            y_pos = newY;
+        }
+    }
+}
