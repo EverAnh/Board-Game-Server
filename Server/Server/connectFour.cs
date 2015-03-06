d52d372b1664a13a0c64eb3853e4bb14a3a901f8
Jason Chiu <jason.chiu@vartyrcore.link>
2015-03-06 12:31:20 -0800

    ConnectFour placeholder

---
+++ Server/Server/connectFour.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class ConnectFour : GenericGame
+    {
+        
+    	// Class variables
+        GenericGame connectFour
+
+    	ConnectFour()
+    	{
+    		connectFour = new GenericGame(8,8);
+    	}
+
+
+
+    
+
+
+
+    }
+
+
+
+
+
+}
+
+
+
+// initialize a
+
+
+// placeGamePiece() 
