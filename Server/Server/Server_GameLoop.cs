1fb536cf5500829dd8d6b2d143bb6d8766b47184
foreza <jforeza@gmail.com>
2015-03-07 16:40:08 -0800

    I evidently don't know how to rename.

---
+++ Server/Server/Server_GameLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Server_GameLoop
+    {
+        public Server_GameLoop()
+        {
+
+        }
+
+        public void gameLoop()
+        {
+            // needs number of players
+            // needs the Player object for each player in the game
+            //      which includes stream reader/writer
+            // 
+        }
+    }
+}
